ec00e3c756eb0c4b5b39c389838e9e009a2c0237
OlyaKorabliova <korablyova@ucu.edu.ua>
2017-09-21 14:17:08 +0300

    Update text_classification.py

--- text_classification.py
+++ text_classification.py
@@ -7,16 +7,6 @@ with open("all_words.json") as g:
 
 with open("my_classifier.pickle", 'rb') as load_pickle:
     classifier = pickle.load(load_pickle)
-
-    '''print(nltk.classify.accuracy(classifier, test_set))
-    classifier.show_most_informative_features(5)
-
-    test_sentence = "Затримано 24 фігуранта, веземо до Києва"
-    test_sentence2 = "Україна повертається до європейської родини"
-    test_sent_features = {word.lower(): (word in nltk.word_tokenize(test_sentence.lower())) for word in all_words}
-    test_sent_features2 = {word.lower(): (word in nltk.word_tokenize(test_sentence2.lower())) for word in all_words}
-    print(classifier.classify(test_sent_features))
-    print(classifier.classify(test_sent_features2))'''
     
     print(classifier.classify("Затримано 24 фігуранта, веземо до Києва"))
     print(classifier.classify("Україна повертається до європейської родини"))
